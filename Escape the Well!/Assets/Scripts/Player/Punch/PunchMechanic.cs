c786fda25b6daaaefad8e346710bd7d88e3c80cb
Marco <mravelo5874@tamu.edu>
2019-02-20 14:58:12 -0600

    Fixed player hitboxes, punch mechanic and added powerpoint

---
+++ Escape the Well!/Assets/Scripts/Player/Punch/PunchMechanic.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PunchMechanic : MonoBehaviour
+{
+    private PlayerController controller;
+    private InputManager im;
+    public Animator anim;
+
+    private bool isPunching = false;
+    public float punch_duration;
+    private float punch_timer;
+
+    [Range(0, 2000)]
+    public float knockbackForce;
+
+    private void Awake()
+    {
+        im = GameObject.Find("InputManager").GetComponent<InputManager>();
+        controller = GetComponent<PlayerController>();
+    }
+
+    private void Update()
+    {
+        if (!isPunching)
+        {
+            // able to punch
+            if (im.Punch(controller.PlayerNum))
+            {
+                isPunching = true;
+                anim.Play("PlayerPunchAnimation");
+            }     
+        }
+        else
+        {
+            punch_timer += Time.deltaTime;
+            if (punch_timer >= punch_duration)
+            {
+                isPunching = false;
+                punch_timer = 0f;
+            }
+        }
+    }
+
+    void OnDrawGizmosSelected()
+    {
+    }
+}
