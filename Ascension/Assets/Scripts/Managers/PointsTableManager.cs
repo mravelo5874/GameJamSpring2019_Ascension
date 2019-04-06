e4560a8006c51d8cf2b4c3a0835de4c3f0855555
Marco <mravelo5874@tamu.edu>
2019-04-06 12:37:11 -0500

    small updates to game loop

---
+++ Ascension/Assets/Scripts/Managers/PointsTableManager.cs
@@ -0,0 +1,92 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class PointsTableManager : MonoBehaviour
+{
+    public GameObject node_prefab;
+    public GameObject VBox_object;
+
+    public float switch_scene_delay;
+    public bool playerWin;
+
+    private void Awake()
+    { 
+        // set up bars
+        for (int i=0; i<StaticVariables.i.playerCount; i++)
+        {
+            GameObject node = Instantiate(node_prefab, VBox_object.transform);
+            BarManager bm = node.GetComponentInChildren<BarManager>();
+            SetColor(bm, i);
+
+            bm.SetName(StaticVariables.i.playerList[i].pc.ToString());
+            bm.SetPointRangeToLerp(StaticVariables.i.playerList[i].total_points, StaticVariables.i.playerList[i].delta_points);
+
+            // if player is gaining points
+            if (StaticVariables.i.playerList[i].delta_points > 0)
+            {
+                bm.SetPointBarScale((float)StaticVariables.i.playerList[i].total_points / StaticVariables.i.pointCount);
+                Debug.Log((float)StaticVariables.i.playerList[i].total_points / StaticVariables.i.pointCount);
+                bm.SetDeltaBarScale((float)(StaticVariables.i.playerList[i].total_points + StaticVariables.i.playerList[i].delta_points) / StaticVariables.i.pointCount);
+                bm.LerpPointBarOverTime(1f, 1f);
+            }
+            else // player is loosing points
+            {
+                bm.SetDeltaBarScale((float)StaticVariables.i.playerList[i].total_points / StaticVariables.i.pointCount);
+                bm.SetPointBarScale((float)(StaticVariables.i.playerList[i].total_points + StaticVariables.i.playerList[i].delta_points) / StaticVariables.i.pointCount);
+                bm.LerpDeltaBarOverTime(1f, 1f);
+            }
+
+            // change point values and reset delta points
+            StaticVariables.i.playerList[i].total_points += StaticVariables.i.playerList[i].delta_points;
+            if (StaticVariables.i.playerList[i].total_points < 0)
+            {
+                StaticVariables.i.playerList[i].total_points = 0;
+            }
+            StaticVariables.i.playerList[i].delta_points = 0;
+        }
+
+
+        // check to see if any player won
+        for (int i = 0; i < StaticVariables.i.playerCount; i++)
+        {
+
+        }
+        if (playerWin)
+        {
+
+        }
+        else
+        {
+            StartCoroutine(SwitchScenes());
+        }
+    }
+
+    private IEnumerator SwitchScenes()
+    {
+        yield return new WaitForSeconds(switch_scene_delay);
+        SceneManager.LoadScene("LevelScene");
+    }
+
+    private void SetColor(BarManager bm, int num)
+    {
+        PlayerVisual.PlayerColor player_color = StaticVariables.i.playerList[num].color;
+        if (player_color == PlayerVisual.PlayerColor.Blue)
+        {
+            bm.SetPointBarColor(Color.blue);
+        }
+        else if (player_color == PlayerVisual.PlayerColor.Red)
+        {
+            bm.SetPointBarColor(Color.red);
+        }
+        else if (player_color == PlayerVisual.PlayerColor.Green)
+        {
+            bm.SetPointBarColor(Color.green);
+        }
+        else if (player_color == PlayerVisual.PlayerColor.Yellow)
+        {
+            bm.SetPointBarColor(Color.yellow);
+        }
+    }
+}
---
+++ Ascension/Assets/Scripts/Player/PlayerVisual.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerVisual : MonoBehaviour
+{
+    public enum PlayerColor { Blue, Red, Green, Yellow };
+
+    public PlayerVisual.PlayerColor playerColor;
+    public Color color;
+
+    private void Awake()
+    {
+        SetPlayerColor();
+    }
+
+    private void SetPlayerColor()
+    {
+        if (playerColor == PlayerVisual.PlayerColor.Red)
+        {
+            color = Color.red;
+        }
+        else if (playerColor == PlayerVisual.PlayerColor.Blue)
+        {
+            color = Color.blue;
+        }
+        else if (playerColor == PlayerVisual.PlayerColor.Green)
+        {
+            color = Color.green;
+        }
+        else if (playerColor == PlayerVisual.PlayerColor.Yellow)
+        {
+            color = Color.yellow;
+        }
+    }
+
+    public static Color ReturnRGB(PlayerVisual.PlayerColor _color)
+    {
+        if (_color == PlayerVisual.PlayerColor.Blue)
+        {
+            return Color.blue;
+        }
+        else if (_color == PlayerVisual.PlayerColor.Red)
+        {
+            return Color.red;
+        }
+        else if (_color == PlayerVisual.PlayerColor.Green)
+        {
+            return Color.green;
+        }
+        else if (_color == PlayerVisual.PlayerColor.Yellow)
+        {
+            return Color.yellow;
+        }
+        return Color.gray;
+    }
+}
