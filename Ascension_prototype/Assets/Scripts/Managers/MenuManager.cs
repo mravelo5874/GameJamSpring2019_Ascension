c2f6b8b910b854888c5a1efd69b1afcdcfcd5254
Marco <mravelo5874@tamu.edu>
2019-04-25 10:16:16 -0500

    Working Final Version

---
+++ Ascension_prototype/Assets/Scripts/Managers/MenuManager.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+
+public class MenuManager : MonoBehaviour
+{
+    private void Start()
+    {
+        StaticVariables.i.musicManager.playTitleSong();
+    }
+
+    public void PlayGame()
+    {
+        SceneManager.LoadScene("GameOptionsScene");
+        StaticVariables.i.sfxManager.play_cool_select();
+    }
+
+    public void Settings()
+    {
+        StaticVariables.i.sfxManager.play_cancel();
+    }
+
+    public void ExitGame()
+    {
+        StaticVariables.i.sfxManager.play_cancel();
+        Application.Quit();
+    }
+}
